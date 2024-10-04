c24ed096497a5dd339116506608209d5e4e03fc8
warra <warrier.abhishek@gmail.com>
2024-10-03 21:54:03 -0400

    feat: add sam2 installation to docker file (increases build time) + all docker scripts now run from root

--- docker/get_ckpts.sh
+++ docker/get_ckpts.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Get the current directory
 current_dir=$(pwd)
 
-# Check if the current directory is parent/docker/
-if [[ "$current_dir" == */docker ]]; then
-    # If true, change directory and run the script
-    cd ../src/segment-anything-2/checkpoints && ./download_ckpts.sh
+if [[ "$current_dir" == */tekkneeca]]; then
+    cd src/segment-anything-2/checkpoints && ./download_ckpts.sh
 else
-    echo "The script must be run from /docker directory."
+    echo "The script must be run from root directory."
     exit 1
 fi
