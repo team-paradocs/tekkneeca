bf2f60a876e81e9199945f0521cab18fcd66766b
liver121888 <liver121888@gmail.com>
2024-10-16 17:34:34 -0400

    Update docker scripts.

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $(pwd)/../src:/ros_ws/src --network host $(for device in /dev/video*; do echo --device=$device:$device; done) --device=/dev/ttyACM0:/dev/ttyACM0 paradockerimage
+sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $(pwd)/src:/ros_ws/src --network host $(for device in /dev/video*; do echo --device=$device:$device; done) --device=/dev/ttyACM0:/dev/ttyACM0 --gpus all paradockerimage:hybrid-planning
