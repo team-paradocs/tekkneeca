904b56f6fdcd75db0b60d0794091bfad9b93d90b
liver121888 <liver121888@gmail.com>
2024-11-03 16:15:42 -0500

    Remove docker-compose, rename no_arduino->camera_only, update camera_only to /src

--- docker/docker_run_no_arduino.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $(pwd)/../src:/ros_ws/src --network host $(for device in /dev/video*; do echo --device=$device:$device; done) --gpus all paradockerimage
