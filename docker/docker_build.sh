f448a78960f1b851d57c9960b7f1a4a0f2550805
warra <warrier.abhishek@gmail.com>
2024-11-05 00:22:31 -0500

    fix: tag names can now work with branch names with symbols

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,2 +1,4 @@
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-sudo docker build -f docker/Dockerfile -t paradockerimage:${BRANCH_NAME} .
+# Replace any non-alphanumeric characters with a hyphen, making it Docker tag friendly
+MODIFIED_BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9_\-]/-/g')
+sudo docker build -f docker/Dockerfile -t paradockerimage:${MODIFIED_BRANCH_NAME} .
