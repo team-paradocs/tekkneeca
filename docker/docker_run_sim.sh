7575d81eb3e83f13052940b193a87ccc5375cebc
Abhishek Warrier <warrier.abhishek@gmail.com>
2024-11-07 17:02:36 -0500

    feat: dockerization v3 (#5)
    * fix: tag names can now work with branch names with symbols
    
    * refactor: move bashrc ROS2 setup +aliases to a separate configuration file for convenience
    
    * feat: add conditional sourcing of workspace
    
    * fix: fix tek_kill alias use of quotes and made it cleaner to read
    
    * feat: cleanup and reorder dockerfile for cache efficiency
    
    * feat: improve usability with oh-my-bash plus zsh like autocomplete
    
    * chore: add docker prefix
    
    * feat: add unified script
    
    * fix: add tf transformations
    
    * feat: colcon build on startup, README update, minor fixes
    
    * fix: update tek_kill alias
    
    * fix: remove serial

--- docker/docker_run_sim.sh
+++ docker/docker_run_sim.sh
@@ -1,2 +1,3 @@
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $(pwd)/src:/ros_ws/src --network host --gpus all paradockerimage:${BRANCH_NAME}
+MODIFIED_BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9_\-]/-/g')
+sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $(pwd)/src:/ros_ws/src --network host --gpus all paradockerimage:${MODIFIED_BRANCH_NAME}
